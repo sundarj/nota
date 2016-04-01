699ecb196f58dd80aa42326e03091018677fd4ab
SundarJ <sundarjoshi@hotmail.co.uk>
2016-04-01 23:29:56 +0100

    refactor; use a uuid for each nota and make every change reactive

--- use/scripts/util.js
+++ use/scripts/util.js
@@ -14,4 +14,16 @@
       lower( keymap[event.keyCode] )
     )
   }
+  
+  u.uid = function() {
+    function s(n) { return h((Math.random() * (1<<(n<<2)))^Date.now()).slice(-n); }
+    function h(n) { return (n|0).toString(16); }
+    return  [
+        s(4) + s(4), s(4),
+        '4' + s(3),                    // UUID version 4
+        h(8|(Math.random()*4)) + s(3), // {8|9|A|B}xxx
+        // s(4) + s(4) + s(4),
+        Date.now().toString(16).slice(-10) + s(2) // Use timestamp to avoid collisions
+    ].join('-');
+}
 }.call(this))
