130944afad3f1a0006641daffb8f412b24506233
Sundar Joshi <sundarjoshi@acceleratedigital.com>
2016-05-17 18:09:24 +0100

    add data-open attr to matched folders; move initial state to Root

--- src/components/nota-list.vue
+++ src/components/nota-list.vue
@@ -2,57 +2,76 @@
   <nav am-nota=list>
     <div am-nota=list-item
       v-for='item of items'
+      v-bind='{ "data-open": isOpen(item) }'
       data-type='{{ item.$.type }}'
     >
         <a v-link='getLink(item)' v-if='item.title'>
           <span am-nota=list-item-title v-text='item.title'>
         </a>
       </span>
-      
+
       <nota-list v-if='item.$.type === "folder"' :items='item.contents'></nota-list>
     </div>
   </nav>
 </template>
 
 <script>
+  import nota from '../app'
+
   export default {
     name: 'nota-list',
     props: [ 'items' ],
-    
+
+    data() {
+      return {
+        nota,
+      }
+    },
+
     created() {
       if ( ! this.items ) this.items = this.initial
     },
-    
+
     methods: {
       getLink({ $ }) {
         const $type = $.type
         const $id = $.id
-        
+
         const isFolder = ( $type === 'folder' )
         const isNota = ( $type === 'nota' )
-        
+
         console.assert(
           isFolder || isNota,
           'item should be either `folder` or  `nota`'
         )
-        
+
         const params = {
           folder: null,
           id: null,
         }
-        
+
         if (isFolder) {
           params.folder = $id
         } else if (isNota) {
           params.folder = $.parent
           params.id = $id
         }
-        
+
         return {
           name: isFolder ? 'list' : 'edit',
           params,
         }
-      }
+      },
+
+      isOpen({ $ }) {
+        if ( $.type !== 'folder' ) return false
+
+        const $folder = this.nota.context.params.folder
+
+        return (
+         $folder === '*' || $folder == $.id
+        )
+      },
     }
   }
 </script>
@@ -62,12 +81,12 @@
   [am-nota=list-item] {
     display block
   }
-  
+
   [data-type=folder] > [am-nota=list-item-title]:first-child::before {
     content '- '
     margin-left -1ch
   }
-  
+
   [am-nota=list-item] {
     margin .5em
   }
--- src/state/list.js
+++ src/state/list.js
@@ -7,92 +7,11 @@ app.add('list', {
   parent: 'root',
   component: NotaList,
 
-  enter(context) {
-    context.data.initial = [{
-      $: {
-        type: 'folder',
-        id: '*',
-      },
-      contents: [{
-        $: {
-          type: 'nota',
-          id: 1,
-          parent: '*',
-        },
-        title: 'The Silmarillion',
-        contents: [ 'duup', 'lex' ],
-      }, {
-        $: {
-          type: 'nota',
-          id: 2,
-          parent: '*',
-        },
-        title: '不思議の国のアリス',
-        contents: [ 'hely' ],
-      }, {
-        $: {
-          type: 'nota',
-          id: 3,
-          parent: '*',
-        },
-        title: 'Warning!!',
-        contents: [ 'tropic' ],
-      }],
-    }, {
-      $: {
-        type: 'folder',
-        id: 4,
-        parent: null,
-      },
-      title: 'other',
-      contents: [{
-        $: {
-          type: 'nota',
-          id: 5,
-          parent: 4,
-        },
-        title: 'Don\'t Panic',
-        contents: [ 'yuna' ],
-      }, {
-        $: {
-          type: 'folder',
-          id: 6,
-          parent: 4,
-        },
-        title: 'else',
-        contents: [{
-          $: {
-            type: 'nota',
-            id: 7,
-            parent: 6,
-          },
-          title: 'Sometimes',
-          contents: [ 'yuna' ],
-        }, {
-          $: {
-            type: 'folder',
-            id: 8,
-            parent: 6,
-          },
-          title: 'another',
-          contents: [{
-            $: {
-              type: 'nota',
-              id: 9,
-              parent: 8,
-            },
-            title: '1',
-            contents: [ 'yuna' ],
-          }],
-        }],
-      }],
-    }]
-  },
   path: '/:folder',
 })
 
 app.add('edit', {
   parent: 'list',
   component: NotaEditor,
-  path: '/:folder/:id',
+  path: ':id',
 })
