e48d02690d193ea5192a5fdcd7264689f4d2b3dc
SundarJ <sundarjoshi@hotmail.co.uk>
2016-07-09 01:52:05 +0100

    allow _ arrow function param in eslintrc

--- src/main.js
+++ src/main.js
@@ -5,7 +5,7 @@ Vue.use( Router )
 import NotaApp from './components/nota-app.vue'
 
 
-const nota = window.nota = Router({
+window.nota = Router({
   routes: [
     { name: 'root', path: '/:id' },
     { name: 'item', path: '/:path*/:id' },
@@ -13,7 +13,7 @@ const nota = window.nota = Router({
 
   app: _ => new Vue({
     el: 'main',
-    
-    render: h => h( NotaApp )
-  })
+
+    render: h => h( NotaApp ),
+  }),
 })
